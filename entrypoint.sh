63016c23174029d299efb365072c7e74b9398557
Abe Tobing <abedzilla@gmail.com>
2019-02-25 18:12:58 +0700

    Version 0.9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-useradd -r -u 1001 -g proxy proxy
-
 create_log_dir() {
   mkdir -p ${SQUID_LOG_DIR}
   chmod -R 755 ${SQUID_LOG_DIR}
@@ -14,20 +12,27 @@ create_cache_dir() {
   chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
 }
 
-create_user() {
-  htpasswd -bc /etc/passwd "${SQUID_AUTH_USER}" "${SQUID_AUTH_PASSWORD}"
-  exec squid3 -N $*
+create_auth_user() {
+  htpasswd -bc /etc/squid3/passwd "${SQUID_AUTH_USER}" "${SQUID_AUTH_PASSWORD}"
+}
+
+apply_backward_compatibility_fixes() {
+  if [[ -f /etc/squid3/squid.user.conf ]]; then
+    rm -rf /etc/squid3/squid.conf
+    ln -sf /etc/squid3/squid.user.conf /etc/squid3/squid.conf
+  fi
 }
 
 create_log_dir
 create_cache_dir
-create_user
+create_auth_user
+apply_backward_compatibility_fixes
 
-# allow arguments to be passed to squid
+# allow arguments to be passed to squid3
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
+elif [[ ${1} == squid3 || ${1} == $(which squid3) ]]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
@@ -36,10 +41,10 @@ fi
 if [[ -z ${1} ]]; then
   if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
     echo "Initializing cache..."
-    $(which squid) -N -f /etc/squid/squid.conf -z
+    $(which squid3) -N -f /etc/squid3/squid.conf -z
   fi
-  echo "Starting squid..."
-  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  echo "Starting squid3..."
+  exec $(which squid3) -f /etc/squid3/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
   exec "$@"
 fi
